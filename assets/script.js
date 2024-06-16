d3822b475c34a60f66af4fdab5c2a610d00d034c
chris-hamamoto <114964118+chris-hamamoto@users.noreply.github.com>
2024-06-16 16:42:10 +0900

    changing toggle

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 document.querySelector('#darkmode').addEventListener('click', function() {
       const modeSwitch = document.querySelector('#darkmode');
       document.getElementsByTagName('body')[0].classList.toggle('dark');
-      if (modeSwitch.textContent === "🌞") {
-            modeSwitch.textContent = "🌝";
-            // modeSwitch.style.backgroundColor = 'black';
-      } else {
-            modeSwitch.textContent = "🌞";
-            // modeSwitch.style.backgroundColor = 'brown';
-      }
+      // if (modeSwitch.textContent === "🌞") {
+      //       modeSwitch.textContent = "🌝";
+      //       modeSwitch.style.backgroundColor = 'black';
+      // } else {
+      //       modeSwitch.textContent = "🌞";
+      //       modeSwitch.style.backgroundColor = 'brown';
+      // }
 });
 
 
