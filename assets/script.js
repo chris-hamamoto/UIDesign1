fa6f0bd92add78ca4ccf43d1b52d28616ff9d29d
chris-hamamoto <chamamoto@snu.ac.kr>
2026-05-26 18:01:11 +0900

    adding pages

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+document.querySelector('#darkmode').addEventListener('click', function() {
+      document.getElementsByTagName('body')[0].classList.toggle('dark');
+});
+
+// window.addEventListener("load", function () {
+//       var hue = Math.floor(Math.random() * 360);
+//       document.body.style.backgroundColor = "hsl(" + hue + ", 80%, 75%)";
+// });
+
+// document.addEventListener("click", function () {
+//       var hue = Math.floor(Math.random() * 360);
+//       document.body.style.backgroundColor = "hsl(" + hue + ", 80%, 75%)";
+// });
+
+// // a single line commment
+// /* mult
+// i line
+// commment */
